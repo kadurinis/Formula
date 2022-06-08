9b06319cbdfc9f7b3e3781608b85dec01bdfd70f
Igor Kadurin <kadurinis@gmail.com>
2022-06-08 13:40:46 +0300

    improve font size

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -18,7 +18,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <style>main, header, footer, nav, div, a, td {font-size: 20px; font-weight: 300}</style>
+    <style>table td {font-size: 24pt; font-weight: 300}</style>
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
