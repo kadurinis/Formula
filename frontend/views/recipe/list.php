29519c5923720fd0f8ce29a7e49e6c92f36482e8
Igor Kadurin <kadurinis@gmail.com>
2022-06-10 13:53:01 +0300

    fix width of buttons in list

--- frontend/views/recipe/list.php
+++ frontend/views/recipe/list.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 /**
  * @var array $list
  */
-$this->registerCss('a.item {min-width: 10em; padding: 20px 5px; margin: 5px 5px; font-size: 1.5rem}');
+$this->registerCss('a.item {min-width: 10em; padding: 20px 5px; margin: 5px 5px; font-size: 1.5rem; width:22%;}');
 $this->title = 'Основной экран';
 ?>
 <div style="display: flex; flex-flow: row wrap; justify-content: space-around; align-content: flex-start; margin-top: 25px">
